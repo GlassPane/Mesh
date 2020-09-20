10b53a02dec232f52449a13305dff03f069496df
UpcraftLP <j-schirra@gmx.de>
2020-09-20 18:49:51 +0200

    fix 'since' numbers

--- src/main/java/io/github/glasspane/mesh/api/MeshApiOptions.java
+++ src/main/java/io/github/glasspane/mesh/api/MeshApiOptions.java
@@ -35,28 +35,28 @@ public final class MeshApiOptions {
     /**
      * Fabric's flag for determining whether we are in a development environment
      *
-     * @since 1.0.2
+     * @since 0.1
      */
     public static final boolean FABRIC_DEVELOPMENT_ENVIRONMENT = Boolean.getBoolean("fabric.development");
 
     /**
      * whether or not we are on a physical client
      *
-     * @since 2.0
+     * @since 0.1
      */
     public static final boolean CLIENTSIDE_ENVIRONMENT = FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
 
     /**
      * global debug flag. if this is enabled, all other debug flags will be.
      *
-     * @since 1.0.2
+     * @since 0.1
      */
     public static final boolean DEBUG_MODE = Boolean.getBoolean("mesh.debug");
 
     /**
      * switch for setting loggers created by the {@link MeshLoggerFactory} to the level defined in {@link MeshApiOptions#DEBUG_LOG_LEVEL}
      *
-     * @since 1.0.2
+     * @since 0.1
      */
     public static final boolean DEBUG_LOGGING_ENABLED = DEBUG_MODE || Boolean.getBoolean("mesh.debug.logging");
 
@@ -64,35 +64,43 @@ public final class MeshApiOptions {
      * used to set the log level for all our Loggers.
      *
      * @see MeshLoggerFactory
-     * @since 1.0.2
+     * @since 0.1
      */
     public static final Level DEBUG_LOG_LEVEL = Level.toLevel(System.getProperty("mesh.debug.logging.level"), FABRIC_DEVELOPMENT_ENVIRONMENT ? Level.ALL : Level.DEBUG);
 
     /**
+     * whether or not to output a full data dump
+     * this includes all registries and their values, as well as tags
+     *
+     * @since 0.4
+     */
+    public static final boolean CREATE_DATA_DUMP = DEBUG_MODE || Boolean.getBoolean("mesh.debug.datadump");
+
+    /**
      * whether or not to output a data dump of virtually injected resources
      *
-     * @since 1.0.2
+     * @since 0.4
      */
-    public static final boolean CREATE_VIRTUAL_DATA_DUMP = DEBUG_MODE || Boolean.getBoolean("mesh.debug.virtual.datadump");
+    public static final boolean CREATE_VIRTUAL_DATA_DUMP = CREATE_DATA_DUMP || Boolean.getBoolean("mesh.debug.datadump.virtual");
 
     /**
      * whether or not to display the number of slots in GUIs
      *
-     * @since 1.1.0
+     * @since 0.1
      */
     public static final boolean RENDER_SLOT_NUMBERS = DEBUG_MODE || Boolean.getBoolean("mesh.debug.render.slotnumber");
 
     /**
      * global switch to disable all vanity features
      *
-     * @since 1.0.2
+     * @since 0.1
      */
     public static final boolean VANITY_FEATURES_ENABLED = !Boolean.getBoolean("mesh.debug.vanity.disabled");
 
     /**
      * if enabled, will enable additional debugging for vanity features
      *
-     * @since 1.0.2
+     * @since 0.1
      */
     public static final boolean VANITY_DEBUG = VANITY_FEATURES_ENABLED && (DEBUG_MODE || Boolean.getBoolean("mesh.debug.vanity"));
 
