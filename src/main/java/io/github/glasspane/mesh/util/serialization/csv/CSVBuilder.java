d75cce7b3efa2ec82af7b6b2dcc5d5438308d3c8
UpcraftLP <j-schirra@gmx.de>
2020-09-23 18:07:55 +0200

    fix csv builder

--- src/main/java/io/github/glasspane/mesh/util/serialization/csv/CSVBuilder.java
+++ src/main/java/io/github/glasspane/mesh/util/serialization/csv/CSVBuilder.java
@@ -1,3 +1,20 @@
+/*
+ * Mesh
+ * Copyright (C) 2019-2020 GlassPane
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; If not, see <https://www.gnu.org/licenses>.
+ */
 package io.github.glasspane.mesh.util.serialization.csv;
 
 import com.google.common.base.MoreObjects;
@@ -8,7 +25,6 @@ import java.io.Writer;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CSVBuilder {
 
@@ -24,7 +40,7 @@ public class CSVBuilder {
 
     public static CSVBuilder create(String firstColumn, String... otherColumns) {
         String[] columns = new String[otherColumns.length + 1];
-        System.arraycopy(otherColumns, 0, columns, 1, columns.length);
+        System.arraycopy(otherColumns, 0, columns, 1, otherColumns.length);
         columns[0] = firstColumn;
         return new CSVBuilder(columns);
     }
