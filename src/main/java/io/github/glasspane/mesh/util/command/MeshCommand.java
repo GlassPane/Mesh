db964ccf29ad7143dc502309174a973079d610c1
Joshua Schirra <j-schirra@gmx.de>
2021-01-05 21:58:44 +0100

    update copyright header

--- src/main/java/io/github/glasspane/mesh/util/command/MeshCommand.java
+++ src/main/java/io/github/glasspane/mesh/util/command/MeshCommand.java
@@ -1,6 +1,6 @@
 /*
  * Mesh
- * Copyright (C) 2019-2020 GlassPane
+ * Copyright (C) 2019-2021 GlassPane
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
